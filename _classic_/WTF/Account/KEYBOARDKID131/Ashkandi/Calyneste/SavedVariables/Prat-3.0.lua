e42e6c29b8726cd6cd4d0862bb788920d720e350
nickmarotta <nickmarotta31@gmail.com>
2022-07-08 20:03:37 -0400

    classic addons from laptop

---
+++ _classic_/WTF/Account/KEYBOARDKID131/Ashkandi/Calyneste/SavedVariables/Prat-3.0.lua
@@ -0,0 +1,214 @@
+
+Prat3CharDB = {
+	["history"] = {
+		["cmdhistory"] = {
+			["ChatFrame10EditBox"] = {
+			},
+			["ChatFrame7EditBox"] = {
+			},
+			["ChatFrame5EditBox"] = {
+			},
+			["ChatFrame8EditBox"] = {
+			},
+			["ChatFrame4EditBox"] = {
+			},
+			["ChatFrame6EditBox"] = {
+			},
+			["ChatFrame9EditBox"] = {
+			},
+			["ChatFrame1EditBox"] = {
+			},
+			["ChatFrame2EditBox"] = {
+			},
+			["ChatFrame3EditBox"] = {
+			},
+		},
+	},
+}
+Prat3HighCPUPerCharDB = {
+	["time"] = 1656093216,
+	["scrollback"] = {
+		["ChatFrame1"] = {
+			{
+				"|cff979797[13:51:53]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Sinofi:1:CHANNEL:4|h|cfff48cbaSinofi|r|h|cffd8d8d8]|r: Is there lore", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				2, -- [8]
+			}, -- [1]
+			{
+				"|cff979797[13:51:57]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Pricecut:2:CHANNEL:4|h|cfff48cbaPricecut|r|h|cffd8d8d8]|r: You tell us why they would make a place called Darkshore Dark/", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				4, -- [8]
+			}, -- [2]
+			{
+				"|cff979797[13:52:17]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Sinofi-Ashkandi:3:CHANNEL:4|h|cfff48cbaSinofi|r|h|cffd8d8d8]|r: Corruption", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				2, -- [8]
+			}, -- [3]
+			{
+				"|cff979797[13:52:41]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Gatherr:4:CHANNEL:4|h|cffaad372Gatherr|r|h|cffd8d8d8]|r: are there any paladins online?", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				6, -- [8]
+			}, -- [4]
+			{
+				"|cff979797[13:52:46]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Deandrie:5:CHANNEL:4|h|cfff48cbaDeandrie|r|h|cffd8d8d8]|r: what is that", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				8, -- [8]
+			}, -- [5]
+			{
+				"|cff979797[13:52:48]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Bluetilateya:6:CHANNEL:4|h|cfffff468Bluetilateya|r|h|cffd8d8d8]|r: there once was a night elf named John Dark. he killed a bunch of stuff, so they named it after him. and he hated light, so they keep the lights down low", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				10, -- [8]
+			}, -- [6]
+			{
+				"|cff979797[13:52:49]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Asukami:7:CHANNEL:4|h|cfff48cbaAsukami|r|h|cffd8d8d8]|r: why", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				12, -- [8]
+			}, -- [7]
+			{
+				"|cff979797[13:52:54]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Zerxus:8:CHANNEL:4|h|cfff48cbaZerxus|r|h|cffd8d8d8]|r: Ew I hope not", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				14, -- [8]
+			}, -- [8]
+			{
+				"|cff979797[13:52:55]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Suncaller:9:CHANNEL:4|h|cfff48cbaSuncaller|r|h|cffd8d8d8]|r: 0", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				16, -- [8]
+			}, -- [9]
+			{
+				"|cff979797[13:52:58]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Exrowdy:10:CHANNEL:4|h|cff3fc6eaExrowdy|r|h|cffd8d8d8]|r: LF enchanter Enchanting: Enchant Boots - Boar's Speed", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				18, -- [8]
+			}, -- [10]
+			{
+				"|cff979797[13:53:12]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Pricecut-Ashkandi:11:CHANNEL:4|h|cfff48cbaPricecut|r|h|cffd8d8d8]|r: I hate paladins", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				4, -- [8]
+			}, -- [11]
+			{
+				"|cff979797[13:53:21]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Hitmehard:12:CHANNEL:4|h|cfff48cbaHitmehard|r|h|cffd8d8d8]|r: Hey !@#$ you buddy", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				20, -- [8]
+			}, -- [12]
+			{
+				"|cff979797[13:53:21]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Zerxus-Ashkandi:13:CHANNEL:4|h|cfff48cbaZerxus|r|h|cffd8d8d8]|r: Same. Smug bastards.", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				14, -- [8]
+			}, -- [13]
+			{
+				"|cff979797[13:53:21]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Blocktalk:14:CHANNEL:4|h|cff3fc6eaBlocktalk|r|h|cffd8d8d8]|r: what", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				22, -- [8]
+			}, -- [14]
+			{
+				"|cff979797[13:53:32]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Pricecut-Ashkandi:15:CHANNEL:4|h|cfff48cbaPricecut|r|h|cffd8d8d8]|r: Zerxus gets it", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				4, -- [8]
+			}, -- [15]
+			{
+				"|cff979797[13:53:33]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Coercion:16:CHANNEL:4|h|cfff48cbaCoercion|r|h|cffd8d8d8]|r: LF tank N Sethekk", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				24, -- [8]
+			}, -- [16]
+			{
+				"|cff979797[13:53:35]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Cptshiftace:17:CHANNEL:4|h|cffff7c0aCptshiftace|r|h|cffd8d8d8]|r: youre only lvl 3.....keep playing wack a mole till 20 then it gets better", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				26, -- [8]
+			}, -- [17]
+			{
+				"|cff979797[13:53:36]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Healingtree:18:CHANNEL:4|h|cffff7c0aHealingtree|r|h|cffd8d8d8]|r: 52 healer lfg brd or st", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				28, -- [8]
+			}, -- [18]
+		},
+	},
+}
---
+++ _classic_/WTF/Account/KEYBOARDKID131/Ashkandi/Corpsel/SavedVariables/Prat-3.0.lua
@@ -0,0 +1,134 @@
+
+Prat3CharDB = {
+	["history"] = {
+		["cmdhistory"] = {
+			["ChatFrame10EditBox"] = {
+			},
+			["ChatFrame7EditBox"] = {
+			},
+			["ChatFrame5EditBox"] = {
+			},
+			["ChatFrame8EditBox"] = {
+			},
+			["ChatFrame4EditBox"] = {
+			},
+			["ChatFrame6EditBox"] = {
+			},
+			["ChatFrame9EditBox"] = {
+			},
+			["ChatFrame1EditBox"] = {
+			},
+			["ChatFrame2EditBox"] = {
+			},
+			["ChatFrame3EditBox"] = {
+			},
+		},
+	},
+}
+Prat3HighCPUPerCharDB = {
+	["time"] = 1656092447,
+	["scrollback"] = {
+		["ChatFrame1"] = {
+			{
+				"|cff979797[13:39:35]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Boostedmoo:1:CHANNEL:4|h|cff3fc6eaBoostedmoo|r|h|cffd8d8d8]|r: fack 120 Burning Blossom away from my damn pet", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				2, -- [8]
+			}, -- [1]
+			{
+				"|cff979797[13:39:53]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Krrpsh:2:CHANNEL:4|h|cffff7c0aKrrpsh|r|h|cffd8d8d8]|r: the pet is just a small disappointing orange whisp", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				4, -- [8]
+			}, -- [2]
+			{
+				"|cff979797[13:40:03]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Zenzie:3:CHANNEL:4|h|cff0070ddZenzie|r|h|cffd8d8d8]|r: lf tank sethekk halls N plzzz", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				6, -- [8]
+			}, -- [3]
+			{
+				"|cff979797[13:40:07]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Boostedmoo-Ashkandi:4:CHANNEL:4|h|cff3fc6eaBoostedmoo|r|h|cffd8d8d8]|r: lies, it's my childhooh memories!", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				2, -- [8]
+			}, -- [4]
+			{
+				"|cff979797[13:40:10]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Sinofi:5:CHANNEL:4|h|cfff48cbaSinofi|r|h|cffd8d8d8]|r: get the sultry dancer", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				8, -- [8]
+			}, -- [5]
+			{
+				"|cff979797[13:40:10]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Twofisch:6:CHANNEL:4|h|cffc69b6dTwofisch|r|h|cffd8d8d8]|r: the cool elemental pet drops from ahune i think", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				10, -- [8]
+			}, -- [6]
+			{
+				"|cff979797[13:40:30]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Boostedmoo-Ashkandi:7:CHANNEL:4|h|cff3fc6eaBoostedmoo|r|h|cffd8d8d8]|r: one does have a chnace to drop from ahune too", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				2, -- [8]
+			}, -- [7]
+			{
+				"|cff979797[13:40:31]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Asukami:8:CHANNEL:4|h|cfff48cbaAsukami|r|h|cffd8d8d8]|r: where do we spend burning blossoms", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				12, -- [8]
+			}, -- [8]
+			{
+				"|cff979797[13:40:38]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Krrpsh-Ashkandi:9:CHANNEL:4|h|cffff7c0aKrrpsh|r|h|cffd8d8d8]|r: major city festivals", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				4, -- [8]
+			}, -- [9]
+			{
+				"|cff979797[13:40:47]|r |Hchannel:channel:4|h[4] |h|cffd8d8d8[|r|Hplayer:Study:10:CHANNEL:4|h|cff3fc6eaStudy|r|h|cffd8d8d8]|r: at the vendor next to the bonfires", -- [1]
+				1, -- [2]
+				0.7529412508010864, -- [3]
+				0.7529412508010864, -- [4]
+				71, -- [5]
+				false, -- [6]
+				1, -- [7]
+				14, -- [8]
+			}, -- [10]
+		},
+	},
+}
